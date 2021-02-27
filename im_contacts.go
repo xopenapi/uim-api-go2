4d7f037bc00f53d9eaf01f0ec60214f8434cef12
whj <82633938@qq.com>
2021-02-27 20:58:18 +0800

    fix

--- im_contacts.go
+++ im_contacts.go
@@ -5,9 +5,9 @@ import (
 )
 
 type IMContact struct {
-	Id            string      `json:"id"`
 	AccountId     string      `json:"accountId"`
 	UserId        string      `json:"userId"`
+	CustomId      string      `json:"customId"`
 	Nickname      string      `json:"nickname"`
 	Avatar        string      `json:"avatar"`
 	Gender        int32       `json:"gender"`
@@ -46,6 +46,7 @@ type IMContactUpateParameters struct {
 type IMContactAddParameters struct {
 	AccountId     string                 `json:"accountId"`
 	UserId        string                 `json:"userId"`
+	CustomId      string                 `json:"customId"`
 	Nickname      string                 `json:"nickname"`
 	Avatar        string                 `json:"avatar"`
 	Gender        int32                  `json:"gender"`
@@ -61,6 +62,26 @@ type IMContactAddParameters struct {
 	ExtendProps   map[string]interface{} `json:"extendProps"`
 }
 
+type IMContactRequestParameters struct {
+	RequestId   string                 `json:"requestId"`
+	AccountId   string                 `json:"accountId"`
+	UserId      string                 `json:"userId"`
+	CustomId    string                 `json:"customId"`
+	Nickname    string                 `json:"nickname"`
+	Avatar      string                 `json:"avatar"`
+	Gender      int32                  `json:"gender"`
+	Mobile      string                 `json:"mobile"`
+	Country     string                 `json:"country"`
+	State       string                 `json:"state"`
+	City        string                 `json:"city"`
+	Signature   string                 `json:"signature"`
+	Alias       string                 `json:"alias"`
+	Tags        []string               `json:"tags"`
+	Source      string                 `json:"source"`
+	ExtendProps map[string]interface{} `json:"extendProps"`
+	Content     string                 `json:"content"`
+}
+
 func (api *Client) ContactAdd(req *IMContactAddParameters) error {
 	return api.ContactAddContext(context.Background(), req)
 }
@@ -68,6 +89,19 @@ func (api *Client) ContactAdd(req *IMContactAddParameters) error {
 func (api *Client) ContactAddContext(ctx context.Context, req *IMContactAddParameters) error {
 	response := UimResponse{}
 	err := api.postJSONMethod(ctx, "contact.add", req, &response)
+	if err != nil {
+		return err
+	}
+	return response.Err()
+}
+
+func (api *Client) ContactRequest(req *IMContactRequestParameters) error {
+	return api.ContactRequestContext(context.Background(), req)
+}
+
+func (api *Client) ContactRequestContext(ctx context.Context, req *IMContactRequestParameters) error {
+	response := UimResponse{}
+	err := api.postJSONMethod(ctx, "contact.request", req, &response)
 	if err != nil {
 		return err
 	}
